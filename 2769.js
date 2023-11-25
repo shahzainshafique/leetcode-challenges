5f9d737306fdfe1f598ab563be6c45a59435ae24
Shahzain Shafique <shahzainshafique1@yahoo.com>
2023-11-25 19:06:48 +0500

    Problem 2769

---
+++ 2769.js
@@ -0,0 +1,16 @@
+/**
+ * You are given two integers, num and t.
+
+An integer x is called achievable if it can become equal to num after applying the following operation no more than t times:
+
+Increase or decrease x by 1, and simultaneously increase or decrease num by 1.
+Return the maximum possible achievable number. It can be proven that there exists at least one achievable number.
+ */
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    return num+(2*t);
+ };
