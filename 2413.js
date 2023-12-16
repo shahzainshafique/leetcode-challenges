4d90775648d7db6ef05b313ee0487f231ddc79e4
Shahzain Shafique <shahzainshafique1@yahoo.com>
2023-12-16 22:33:26 +0500

    Problem 2413

---
+++ 2413.js
@@ -0,0 +1,10 @@
+/**
+ * Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+    return n % 2 == 0 ? n: n*2;
+  };
