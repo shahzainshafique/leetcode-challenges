dfcd387fe05b65f53da2db8a9bf06495c908b480
Shahzain Shafique <shahzainshafique1@yahoo.com>
2023-12-31 16:18:46 +0500

    Problem 217

---
+++ 217.js
@@ -0,0 +1,16 @@
+/**
+ * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ */
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    nums.sort((a,b)=>a-b);
+    for(let i = 0;i<nums.length;i++){
+        if(nums[i]==nums[i+1]){
+            return true;
+        }
+    }
+    return false;
+};
