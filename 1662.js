4f41050349507bed55364439c8f3ce133af48078
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-05-13 15:33:07 +0500

    1662

---
+++ 1662.js
@@ -0,0 +1,14 @@
+/**Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+A string is represented by an array if the array elements concatenated in order forms the string.
+
+  */
+
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    return word1.join("") == word2.join("") ? true:false;
+};
