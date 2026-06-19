f7f0e3f57328e0c7c4849c38ccc421cfda637e30
Shahzain Shafique <shahzainshafique1@yahoo.com>
2026-06-19 17:16:07 +0500

    2727

---
+++ 2727.js
@@ -0,0 +1,14 @@
+/*
+Given an object or an array, return if it is empty.
+
+An empty object contains no key-value pairs.
+An empty array contains no elements.
+You may assume the object or array is the output of JSON.parse. */
+
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    return ( Object.keys(obj).length === 0 && Object.values(obj).length === 0 ) || (Array.isArray(obj) && obj.length === 0)
+};
